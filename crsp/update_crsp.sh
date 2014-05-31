02150a1e27399b9881b24eea503e9ab15ca67deb
Ian Gow <iandgow@gmail.com>
2014-05-31 09:31:56 -0400

    Updated "make CRSP" routine
    Also changed name and syntax of what is now wrds_to_pg.

--- crsp/update_crsp.sh
+++ crsp/update_crsp.sh
@@ -1,24 +1,24 @@
-./get_schema_alt.pl crsp msf
-./get_schema_alt.pl crsp dsf
-./get_schema.pl crsp dport1
-./get_schema.pl crsp dseexchdates
-./get_schema.pl crsp dsi
-./get_schema.pl crsp mport1 
-./get_schema.pl crsp msi
-./get_schema.pl crsp msp500list
-./get_schema.pl crsp stocknames
-./get_schema_alt.pl crsp ccmxpf_linktable
-./get_schema_alt.pl crsp ccmxpf_lnkhist
-./get_schema_alt.pl crsp ccmxpf_lnkused
-./get_schema_alt.pl crsp dsedelist
-./get_schema_alt.pl crsp dsedist
-./get_schema_alt.pl crsp msedelist
-./get_ermport.pl
-./get_erdport.pl
+../wrds_to_pg crsp.msf --fix-missing
+../wrds_to_pg crsp.dsf --fix-missing
+../wrds_to_pg crsp.dport1
+../wrds_to_pg crsp.dseexchdates
+../wrds_to_pg crsp.dsi
+../wrds_to_pg crsp.mport1 
+../wrds_to_pg crsp.msi
+../wrds_to_pg crsp.msp500list
+../wrds_to_pg crsp.stocknames
+../wrds_to_pg crsp.ccmxpf_linktable --fix-missing
+../wrds_to_pg crsp.ccmxpf_lnkhist --fix-missing
+../wrds_to_pg crsp.ccmxpf_lnkused --fix-missing
+../wrds_to_pg crsp.dsedelist --fix-missing
+../wrds_to_pg crsp.dsedist --fix-missing
+../wrds_to_pg crsp.msedelist --fix-missing
+../get_ermport.pl
+../get_erdport.pl
 
-psql -d crsp -f crsp_make_erdport1.sql
-psql -d crsp -f crsp_make_mrets.sql
-psql -d crsp -f crsp_make_rets_alt.sql
-psql -d crsp -f ~/Dropbox/pg_backup/support/permissions.sql 
+psql -f crsp_make_erdport1.sql
+psql -f crsp_make_mrets.sql
+psql -f crsp_make_rets_alt.sql
+psql -f ../permissions.sql 
 
-pg_dump --format custom --no-tablespaces --verbose --file ~/Dropbox/pg_backup/crsp.backup --schema "crsp" "crsp"
+pg_dump --format custom --no-tablespaces --verbose --file ~/Dropbox/pg_backup/crsp.backup --schema "crsp"
