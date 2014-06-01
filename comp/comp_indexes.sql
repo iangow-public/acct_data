d2cab975954a467373139c378cf6f193183a52b0
Ian Gow <igow@hbs.edu>
2014-06-01 14:00:35 -0400

    Remove index code for non-existent table.
    Must test code before pushing.

--- comp/comp_indexes.sql
+++ comp/comp_indexes.sql
@@ -11,7 +11,6 @@ CREATE INDEX ON comp.g_sec_divid (gvkey, datadate);
 CREATE INDEX ON comp.idx_ann (datadate);
 CREATE INDEX ON comp.io_qbuysell (gvkey, datadate);
 CREATE INDEX ON comp.names (gvkey);
-CREATE INDEX ON comp.sec_divid (gvkey, datadate);
 CREATE INDEX ON comp.secm (gvkey, datadate);
 CREATE INDEX ON comp.spind_mth (gvkey, datadate);
 CREATE INDEX ON comp.wrds_segmerged (gvkey, datadate);
