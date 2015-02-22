889b104dbbc2735b1154aa5ac44ba82967cc1d1f
Ian Gow <iandgow@gmail.com>
2015-02-22 11:12:12 -0500

    Added comments to code.
    - Also added code to subset mismatches
    - Saved output to StreetEvents.

--- streetevents/ticker_mismatches.R
+++ streetevents/ticker_mismatches.R
@@ -1,8 +1,11 @@
+# Function that uses agrep() to determine matches
+# I believe agrep() uses Levenshtein distance
 amatch <- function(str1, str2) {
     if (str1=="" | str2 =="") return(NA)
     agrepl(str1, str2) | agrepl(str2, str1)
 }
 
+# Get data from PostgreSQL
 library("RPostgreSQL")
 pg <- dbConnect(PostgreSQL())
 sql <- paste(readLines("streetevents/ticker_mismatches.sql"),
@@ -11,4 +14,19 @@ sql <- paste(readLines("streetevents/ticker_mismatches.sql"),
 sample <- dbGetQuery(pg, sql)
 dbDisconnect(pg)
 
-dim(unique(subset(sample, diff_name, ticker)))
+# Use Levenshtein distance to code matches
+library(parallel)
+sample$amatch_diff <- !(unlist(mcMap(amatch, sample$co_name, 
+                                     sample$original_name, mc.cores=4)))
+
+# Select cases with potential mismatches
+mismatch_sample <- subset(sample, diff_name | amatch_diff)
+mismatch_sample <- mismatch_sample[order(mismatch_sample$ticker), ]
+
+# NUmber of distinct tickers
+length(unique(mismatch_sample$ticker))
+
+# Save to CSV in Google Drive
+write.csv(mismatch_sample, 
+          file="~/Google Drive/streetevents_ticker_mismatches.csv")
+
