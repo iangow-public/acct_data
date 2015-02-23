48629cef957131c88ac12b933d9978046912f5e2
Ian Gow <iandgow@gmail.com>
2015-02-22 20:50:34 -0500

    Add conference calls with non-standard call descriptions.
     - May need to tweak regexes to handle these.

--- streetevents/ticker_mismatches.sql
+++ streetevents/ticker_mismatches.sql
@@ -27,22 +27,23 @@ raw_data AS (
     SELECT file_name, ticker, co_name, call_desc, 
         regexp_replace(call_desc,
                        '^Q[1-4] \d{4} (.*) Earnings Conference Call$',
-                       '\1') AS original_name
+                       '\1') AS original_name,
+        call_desc ~ '^Q[1-4] \d{4} .* Earnings Conference Call$' AS call_desc_std
     FROM streetevents.calls
-    WHERE call_type=1 
-        AND call_desc ~ '^Q[1-4] \d{4} .* Earnings Conference Call$'
-        AND ticker !~ '\.' -- Exclude foreign firms
+    WHERE call_type=1 AND ticker !~ '\.' -- Exclude foreign firms
     ),
 
 clean_names AS (
-    SELECT file_name, ticker, call_desc,
+    SELECT file_name, ticker, call_desc, call_desc_std,
         streetevents.fix_name(co_name) AS co_name,
         streetevents.fix_name(original_name) AS original_name
     FROM raw_data)
     
 SELECT a.*, c.call_date, b.match_type, b.match_type_desc, 
     d.comnam, d.permno, 
-    NOT (a.co_name ~* original_name OR original_name ~* a.co_name) AS diff_name
+    CASE WHEN call_desc_std 
+        THEN a.co_name ~* original_name OR original_name ~* a.co_name
+    END AS diff_name
 FROM clean_names AS a
 INNER JOIN streetevents.calls AS c
 USING (file_name)
