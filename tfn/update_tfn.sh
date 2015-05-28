9b505f6d91763f11ffaf2f8398f76828bec64d65
Ian Gow <igow@hbs.edu>
2015-05-28 11:56:51 -0400

    Added code to get TFN data from WRDS.

---
+++ tfn/update_tfn.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+./wrds_to_pg_v2 tfn.s12
+./wrds_to_pg_v2 tfn.s12type1
+./wrds_to_pg_v2 tfn.s12type2
+./wrds_to_pg_v2 tfn.s34
+./wrds_to_pg_v2 tfn.s34type1
+./wrds_to_pg_v2 tfn.s34type2
