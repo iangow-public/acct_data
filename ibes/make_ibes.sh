97e9e5803a3c52a3ba6a26f7ca3875e0585cfb15
Ian Gow <igow@hbs.edu>
2015-08-23 13:07:58 -0400

    Fixes to IBES code.

--- ibes/make_ibes.sh
+++ ibes/make_ibes.sh
@@ -1,24 +1,25 @@
 #!/usr/bin/env bash 
-R CMD BATCH get_iclink.R
+Rscript ibes/get_iclink.R
 
-./wrds_to_pg_v2 ibes.statsum_epsus
-./wrds_to_pg_v2 ibes.act_epsus
-./wrds_to_pg_v2 ibes.actpsumu_epsus
-./wrds_to_pg_v2 ibes.actu_epsus
-./wrds_to_pg_v2 ibes.detu_epsus
+./wrds_to_pg_v2.pl ibes.statsum_epsus
+./wrds_to_pg_v2.pl ibes.act_epsus
+./wrds_to_pg_v2.pl ibes.actpsumu_epsus
+./wrds_to_pg_v2.pl ibes.actu_epsus
+./wrds_to_pg_v2.pl ibes.detu_epsus
 if [ $? -eq 1 ] ; then
     psql -c "SET maintenance_work_mem='10GB'; CREATE INDEX ON ibes.detu_epsus (ticker, revdats)"
 fi
 
-./wrds_to_pg_v2 ibes.det_epsus
-./wrds_to_pg_v2 ibes.id
-./wrds_to_pg_v2 ibes.idsum
-./wrds_to_pg_v2 ibes.statsum_epsus
-./wrds_to_pg_v2 ibes.statsumu_epsus
+./wrds_to_pg_v2.pl ibes.det_epsus
+./wrds_to_pg_v2.pl ibes.id
+./wrds_to_pg_v2.pl ibes.idsum
+./wrds_to_pg_v2.pl ibes.statsum_epsus
+./wrds_to_pg_v2.pl ibes.statsumu_epsus
 if [ $? -eq 1 ] ; then
     psql -c "SET maintenance_work_mem='10GB'; CREATE INDEX ON ibes.statsumu_epsus (ticker, statpers)"
 fi
 
-./wrds_to_pg_v2 ibes.surpsum
+./wrds_to_pg_v2.pl ibes.surpsum
 
-pg_dump  --format custom --no-tablespaces --verbose --file ~/Dropbox/pg_backup/ibes.backup --schema "ibes" "crsp"
+pg_dump  --format custom --no-tablespaces --verbose \
+    --file $PGBACKUP_DIR/ibes.backup --schema "ibes" 
