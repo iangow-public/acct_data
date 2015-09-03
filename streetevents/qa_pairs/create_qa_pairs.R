0a480cbdad2b61a6ccb03abd275ca68dddabae6e
Ian Gow <iandgow@gmail.com>
2015-09-03 16:25:11 -0400

    Fixed qa_pairs table.
    - Removed unnecessary data
    - Removed unnecessary code.

--- streetevents/qa_pairs/create_qa_pairs.R
+++ streetevents/qa_pairs/create_qa_pairs.R
@@ -5,43 +5,41 @@ pg <- dbConnect(PostgreSQL())
 if (!dbExistsTable(pg, c("streetevents", "qa_pairs"))) {
     dbGetQuery(pg, "
         DROP TABLE IF EXISTS streetevents.qa_pairs;
-    
+
         CREATE TABLE streetevents.qa_pairs
         (
           file_name text,
-          questioner text,
-          questions text[],
-          question_nums integer[],
-          answerers text[],
+          last_update timestamp without time zone,
           answer_nums integer[],
-          answers text[],
-          answerer_roles text[]
+          question_nums integer[]
         );
-                   
-        CREATE INDEX ON streetevents.qa_pairs (file_name);
-        
+
+        CREATE INDEX ON streetevents.qa_pairs (file_name, last_update);
+
         GRANT SELECT ON streetevents.qa_pairs TO personality_access;")
 }
 
 file_list <- dbGetQuery(pg, "
-    SELECT DISTINCT file_name
-    FROM streetevents.speaker_data AS a
-    INNER JOIN streetevents.calls
-    USING (file_name)
-    WHERE call_type=1 AND 
-        file_name NOT IN (SELECT file_name FROM streetevents.qa_pairs)")
+    SET work_mem='3GB';
+
+    SELECT DISTINCT file_name, last_update
+    FROM streetevents.calls AS a
+    WHERE call_type=1 AND
+        (file_name, last_update) NOT IN
+            (SELECT file_name, last_update
+             FROM streetevents.qa_pairs)")
 
 rs <- dbDisconnect(pg)
 
 addQAPairs <- function(file_name) {
     library("RPostgreSQL")
-    sql <- paste(readLines("streetevents/create_qa_pairs.sql"), collapse="\n")
+    sql <- paste(readLines("streetevents/qa_pairs/create_qa_pairs.sql"), collapse="\n")
 
-    pg <- dbConnect(PostgreSQL())    
-    dbGetQuery(pg, sprintf(sql, file_name))
+    pg <- dbConnect(PostgreSQL())
+    dbGetQuery(pg, sprintf(sql, file_name, file_name))
     dbDisconnect(pg)
 }
 
 library(parallel)
-system.time(res <- unlist(mclapply(file_list$file_name, addQAPairs, mc.cores=12)))
+system.time(res <- unlist(mclapply(file_list$file_name, addQAPairs, mc.cores=8)))
 
--- streetevents/qa_pairs/create_qa_pairs.sql
+++ streetevents/qa_pairs/create_qa_pairs.sql
@@ -1,63 +1,58 @@
-INSERT INTO streetevents.qa_pairs 
+DELETE FROM streetevents.qa_pairs WHERE file_name='%s';
+
+INSERT INTO streetevents.qa_pairs (file_name, last_update, question_nums, answer_nums)
 
 WITH sample_transcripts AS (
-    SELECT a.file_name, a.speaker_name, a.speaker_number,
-      trim(a.speaker_text) AS speaker_text, a.role,
-      role != 'Analyst' AS is_answer,
-      role = 'Analyst' AND speaker_text ~ '\?' AS is_question
+    SELECT a.file_name, a.last_update,
+        a.speaker_name, a.speaker_number,
+        trim(a.speaker_text) AS speaker_text, a.role,
+        role != 'Analyst' AS is_answer,
+        role = 'Analyst' AND speaker_text ~ '\?' AS is_question
     FROM streetevents.speaker_data AS a
     INNER JOIN streetevents.calls
-    USING (file_name)
-    WHERE  call_type=1 AND context='qa' AND speaker_name != 'Operator'
+    USING (file_name, last_update)
+    WHERE call_type=1 AND context='qa' AND speaker_name != 'Operator'
         AND file_name='%s'
-    ORDER BY file_name, speaker_number),
+    ORDER BY file_name, last_update, speaker_number),
 
 grouped AS (
-   SELECT *, 
+   SELECT *,
       sum(is_question::integer) OVER all_obs AS question_group
       FROM sample_transcripts
-      WINDOW all_obs AS (PARTITION BY file_name ORDER BY speaker_number)),
+      WINDOW all_obs AS (PARTITION BY file_name, last_update ORDER BY speaker_number)),
 
 questions_raw AS (
-    SELECT file_name, question_group,
+    SELECT file_name, last_update, question_group,
       speaker_name,
       array_agg(speaker_number ORDER BY speaker_number) AS speaker_numbers,
-      array_agg(speaker_text ORDER BY speaker_number) AS speaker_texts,
       bool_or(is_question) AS is_question
     FROM grouped
     WHERE role='Analyst'
-    GROUP BY file_name, question_group, speaker_name),
+    GROUP BY file_name, last_update, question_group, speaker_name),
 
 questions_inter AS (
     SELECT *, lead(speaker_numbers) OVER w AS lead_speaker_numbers
-    FROM questions_raw 
-    WINDOW w AS (PARTITION BY file_name ORDER BY speaker_numbers)),
+    FROM questions_raw
+    WINDOW w AS (PARTITION BY file_name, last_update ORDER BY speaker_numbers)),
 
 questions AS (
-    SELECT file_name, speaker_name AS questioner, speaker_texts AS questions, 
+    SELECT file_name, last_update, speaker_name AS questioner,
         speaker_numbers AS question_nums,
-        array_min(speaker_numbers) AS first_speaker_num, 
-        array_min(lead_speaker_numbers) - 1 AS last_speaker_num 
+        array_min(speaker_numbers) AS first_speaker_num,
+        array_min(lead_speaker_numbers) - 1 AS last_speaker_num
     FROM questions_inter),
 
 answers AS (
-    SELECT a.file_name, b.first_speaker_num,
-        array_agg(speaker_name ORDER BY speaker_number) AS answerers,
-        array_agg(speaker_number ORDER BY speaker_number) AS answer_nums,
-        array_agg(speaker_text ORDER BY speaker_number) AS answers,
-        array_agg(role ORDER BY speaker_number) AS answerer_roles
+    SELECT a.file_name, a.last_update, b.first_speaker_num,
+        array_agg(speaker_number ORDER BY speaker_number) AS answer_nums
     FROM sample_transcripts AS a
     INNER JOIN questions AS b
-    ON a.file_name=b.file_name AND a.speaker_number>=b.first_speaker_num AND 
+    ON a.file_name=b.file_name AND a.speaker_number>=b.first_speaker_num AND
         (a.speaker_number <= b.last_speaker_num OR b.last_speaker_num IS NULL)
     WHERE is_answer
-    GROUP BY a.file_name, b.first_speaker_num)
+    GROUP BY a.file_name, a.last_update, b.first_speaker_num)
 
-SELECT file_name, questioner, questions, question_nums,
-  answerers, answer_nums, answers, answerer_roles
-FROM questions 
+SELECT file_name, last_update, question_nums, answer_nums
+FROM questions
 INNER JOIN answers
-USING (file_name, first_speaker_num) ;
-
-
-
+USING (file_name, last_update, first_speaker_num) ;
