893f81e7062e56ba2950a5d70e68767801c656f0
Ian Gow <igow@hbs.edu>
2015-09-12 14:42:11 -0400

    Added table to ciq schema.

--- ciq/get_ciq.sh
+++ ciq/get_ciq.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 ./wrds_to_pg_v2.pl ciq.wrds_keydev
 
+./wrds_to_pg_v2.pl --fix-missing ciq.wrds_gvkey;
 ./wrds_to_pg_v2.pl --fix-missing ciq.wrds_cusip;
 ./wrds_to_pg_v2.pl --fix-missing ciq.wrds_cik;
 
