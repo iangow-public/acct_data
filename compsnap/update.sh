1f4d95792de5cdb5d51f1bad41f661ba29d61575
Ian Gow <iandgow@gmail.com>
2015-09-23 15:24:55 -0400

    Added code for snapshot Compustat data.

--- compsnap/update.sh
+++ compsnap/update.sh
@@ -5,3 +5,11 @@ if [ $? -eq 1 ] ; then
     psql -f compsnap/make_indexes.sql
 fi
 
+./wrds_update.pl compsnap.wrds_csa_pit
+
+if [ $? -eq 1 ] ; then
+    psql -f compsnap/make_indexes_a.sql
+fi
+
+./wrds_update.pl compsnap.cscompany
+
