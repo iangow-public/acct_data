8697c3ce647cc1d2581d27dd7a95d2e85118551a
Ian Gow <igow@hbs.edu>
2015-09-25 09:17:52 -0400

    Tweaks to Capital IQ code.

--- ciq/update_ciq.sh
+++ ciq/update_ciq.sh
@@ -2,7 +2,10 @@
 ./wrds_update.pl --fix-missing ciq.wrds_gvkey;
 ./wrds_update.pl --fix-missing ciq.wrds_cusip;
 ./wrds_update.pl --fix-missing ciq.wrds_cik;
-
 ./wrds_update.pl ciq.wrds_keydev
 
-psql -f ciq/ciq_indexes.sql
+if [ $? -eq 1 ] ; then
+    psql -f ciq/ciq_indexes.sql
+fi
+
+
