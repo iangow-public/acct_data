703818fdd5f48614ec44e2c5b898ccb26ee098e4
Ian Gow <iandgow@gmail.com>
2015-11-01 11:10:33 -0500

    Update CUSIP-CIK match to use 10-K filings too.

--- director/create_ciks.sql
+++ director/create_ciks.sql
@@ -49,7 +49,7 @@ cusip_matches AS (
 sec_names AS (
     SELECT DISTINCT upper(company_name) AS company_name, cik
     FROM filings.filings
-    WHERE form_type IN ('6-K', 'DEF 14A')),
+    WHERE form_type IN ('6-K', 'DEF 14A', '10-K')),
 
 name_matches AS (
     SELECT equilar_id, a.cusip, a.fy_end, c.cik::integer, year,
