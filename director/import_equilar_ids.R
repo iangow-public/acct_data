50184a98672fb006d0f3d97b1d47e5fcd6607cd0
Ian Gow <igow@hbs.edu>
2015-11-11 07:39:27 -0500

    Added code to import IDs from Equilar.

---
+++ director/import_equilar_ids.R
@@ -0,0 +1,23 @@
+library(readr)
+
+path <- "~/Dropbox/data/equilar/director_match"
+
+director_ids <-
+    read_csv(file.path(path, "director_ids.csv"), na = "#N/A") %>%
+    as.data.frame()
+
+names(director_ids) <- tolower(names(director_ids))
+company_ids <- read_csv(file.path(path, "company_ids.csv"), na = "N/A") %>%
+        as.data.frame()
+names(company_ids) <- tolower(names(company_ids))
+
+library(RPostgreSQL)
+
+pg <- dbConnect(PostgreSQL())
+
+rs <- dbWriteTable(pg, c("director", "director_ids"), director_ids,
+                   row.names = FALSE, overwrite = TRUE)
+rs <- dbWriteTable(pg, c("director", "company_ids"), company_ids,
+                   row.names = FALSE, overwrite = TRUE)
+
+rs <- dbDisconnect(pg)
