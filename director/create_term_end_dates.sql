fe53951583df8b4712fb27ef64a03d4a59acd0ca
Ian Gow <igow@hbs.edu>
2016-01-04 12:45:40 -0500

    Tweak to query for director term-end dates.

--- director/create_term_end_dates.sql
+++ director/create_term_end_dates.sql
@@ -1,7 +1,9 @@
 SET work_mem='1GB';
 
-CREATE TABLE director.term_end_dates AS 
-WITH 
+DROP TABLE IF EXISTS director.term_end_dates;
+
+CREATE TABLE director.term_end_dates AS
+WITH
 
 term_dates AS (
     SELECT director.equilar_id(director_id),
@@ -30,11 +32,11 @@ boardex_dates AS (
     USING (equilar_id, directorid)),
 
 implied_end_dates AS (
-    SELECT b.equilar_id, b.director_id, 
+    SELECT b.equilar_id, b.director_id,
         min(fy_end) AS implied_end_date
     FROM director.co_fin AS a
     INNER JOIN term_dates AS b
-    ON director.equilar_id(company_id) =b.equilar_id 
+    ON director.equilar_id(company_id) =b.equilar_id
         AND a.fy_end > b.last_fy_end
     GROUP BY 1, 2)
 
