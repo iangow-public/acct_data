b6167887d2731b287d4565c17c2801d2c3c20b0e
Ian Gow <iandgow@gmail.com>
2017-04-12 09:24:07 -0400

    Deleted Equilar-related code.

--- equilar/update_equilar_all.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-pg_dump --host localhost --username "igow" --format custom --no-tablespaces --verbose --file ~/Dropbox/pg_backup/board.backup --schema "board" "crsp" --no-owner
-pg_dump --host localhost --username "igow" --format custom --no-tablespaces --verbose --file ~/Dropbox/pg_backup/executive.backup --schema "executive" "crsp" --no-owner
